6dc218f77543c00539ada08f94a2b1968565a8b3
Loïc Bitter <loic.bitter@orange.fr>
2018-11-16 23:31:01 +0100

    Adding files of TPs
    Add all TPs files to the repository.

---
+++ td02/ex1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<head>
+ 
+        <meta charset="utf-8" />
+ 
+    <title></title>
+    <meta name="description" content="">
+ 
+        <link rel="stylesheet" type="text/css" href="" media="screen">
+ 
+        <link rel="shortcut icon" href="">
+</head>
+<body>
+       	<div id="conteneur1">
+          Voici le contenu du conteneur #1. Ce conteneur est un div
+       	</div>
+
+       	<div id="conteneur2">
+          Voici le contenu du conteneur #2. Ce conteneur est un div qui contient du texte et d'autre éléments comme une image 
+          <br />
+          <img src="https://www.thoiry.net/sites/default/files/styles/cbox/public/fa-galerie/grand%20sourir%20panda.jpg?itok=BzujGJnQ" width="300" />
+       	</div>
+</body>
+ 
+</html>
